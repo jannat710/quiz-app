0001934fe6039f3f8355bcc45fe89d5105e659a3
e <jannatul0040@gmail.com>
2025-02-01 23:35:01 +0600

    worked on quiz summary section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Question from "./home/Question";
+import QuizSummary from "./home/QuizSummary";
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
+  const { quizComplete } = useAppSelector((state) => state.quiz);
   return (
     <div>
       <h3 className="text-center font-bold text-4xl my-12">Quiz App</h3>
-      <Question />
+      {!quizComplete ? <Question /> : <QuizSummary />}
     </div>
   );
 }
