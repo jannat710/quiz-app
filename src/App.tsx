e6493e5f30ce3afe41e8a95807361cfac594ad75
e <jannatul0040@gmail.com>
2025-02-02 23:18:36 +0600

    integrate RTK Query API for fetching all quizzes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import AllQuiz from "./home/AllQuiz";
 import Question from "./home/Question";
 import QuizSummary from "./home/QuizSummary";
 import { useAppSelector } from "./redux/hooks";
@@ -5,8 +6,9 @@ import { useAppSelector } from "./redux/hooks";
 function App() {
   const { quizComplete } = useAppSelector((state) => state.quiz);
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <h3 className="text-center font-bold text-4xl my-12">Quiz App</h3>
+      <AllQuiz></AllQuiz>
       {!quizComplete ? <Question /> : <QuizSummary />}
     </div>
   );
