f8c98060fd0ef89d066ab4333ef722fac5b28ea7
e <jannatul0040@gmail.com>
2025-02-04 23:50:46 +0600

    Added quiz tags for caching

--- src/redux/api/quizApi.ts
+++ src/redux/api/quizApi.ts
@@ -2,9 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const quizApi = createApi({
   reducerPath: "quizApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  tagTypes: ["Quiz"],
   endpoints: (builder) => ({
     getAllQuiz: builder.query({
       query: () => `/quizzes`,
+      providesTags: ["Quiz"],
     }),
     addQuizApi: builder.mutation({
       query: (body) => (
@@ -15,6 +17,7 @@ export const quizApi = createApi({
           body,
         }
       ),
+      invalidatesTags: ["Quiz"],
     }),
   }),
 });
